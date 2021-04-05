ff9cf661d25b686407453e34798d60dd0c3bd0bc
Ladislus <walcak.ladislas@gmail.com>
2021-04-05 22:01:08 +0200

    [ADD] Added convolution v1 (detection_bord: OK, flou: NON)

--- convolution/include/convolution.hpp
+++ convolution/include/convolution.hpp
@@ -7,21 +7,32 @@
 
 typedef const std::vector<int>& const_vector_ref;
 typedef std::function<const_mat_ptr(const_mat_ref)> convolution_function;
+typedef std::vector<std::pair<int, int>> vec_pair;
 
 enum Effect {
     FLOU_GAUSSIEN,
+    FLOU_BOX,
     DETECTION_BORD,
 };
 
 const_mat_ptr process(const_mat_ref , const Effect&);
 const_mat_ptr apply(const_mat_ref, const_vector_ref, float, float);
+bool check(int i, int j, int current_coords, int max_row, int max_col);
 
 const_mat_ptr flou_gaussien(const_mat_ref);
+const_mat_ptr flou_box(const_mat_ref);
 const_mat_ptr detection_bord(const_mat_ref);
 
 const std::map<Effect, convolution_function> effect_functions {
         {Effect::FLOU_GAUSSIEN, flou_gaussien},
-        {Effect::DETECTION_BORD, detection_bord}
+        {Effect::DETECTION_BORD, detection_bord},
+        {Effect::FLOU_BOX, flou_box}
+};
+
+const vec_pair coordinates = {
+        {-1, -1}, {-1, 0}, {-1, +1},
+        {0, -1}, {0, 0}, {0, +1},
+        {+1, -1}, {+1, 0}, {+1, +1}
 };
 
 #endif
--- convolution/src/convolution.cpp
+++ convolution/src/convolution.cpp
@@ -8,8 +8,12 @@ int main(int argc, char** argv) {
 
     if (image.empty() || !image.data) missing_data();
 
+    if (image.channels() != 4) throw std::logic_error("No support for image that are not 4 channels yet !");
+
     const_mat_ptr result = process(image, Effect::FLOU_GAUSSIEN);
-    std::cout << result << std::endl;
+    cv::imwrite("convolution_cpu.png", *result);
+
+    delete result;
 
     return 0;
 }
@@ -19,17 +23,65 @@ const_mat_ptr process(const_mat_ref mat, const Effect& effect) {
         return effect_functions.at(effect)(mat);
     } catch (const std::out_of_range& oor) {
         std::cerr << "Error finding corresponding fonction" << std::endl;
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 }
 
 const_mat_ptr apply(const_mat_ref mat, const_vector_ref kernel, float divider, float offset) {
-    // TODO
-    return nullptr;
+
+    auto* candidate = new cv::Mat(mat.rows, mat.cols, CV_8UC(4));
+
+    // Pour chacunes des lignes
+    // (size_t provoque des "narrow conversion")
+    for (int i = 0; i < mat.rows; i++)
+
+        // Pour chacunes de colonnes
+        // (size_t provoque des "narrow conversion")
+        for (int j = 0; j < mat.cols; j++) {
+
+            // Initialisation de la somme
+            int sum_blue = 0;
+            int sum_green = 0;
+            int sum_red = 0;
+
+            // Pour chacun des 9 cases dans son voisinage
+            // (size_t provoque des "narrow conversion")
+            for (int current_neighbor_index = 0; current_neighbor_index < kernel.size(); current_neighbor_index++)
+
+                // Si la case n'est pas hors limite
+                if (check(i, j, current_neighbor_index, mat.rows, mat.cols)) {
+                    int current_factor = kernel[current_neighbor_index];
+                    std::pair<int, int> current_coords = {i + coordinates[current_neighbor_index].first, j + coordinates[current_neighbor_index].second };
+                    cv::Vec4b current_pixel = mat.at<cv::Vec4b>(current_coords.first, current_coords.second);
+
+
+                    sum_blue += current_pixel[0] * current_factor;
+                    sum_green += current_pixel[1] * current_factor;
+                    sum_red += current_pixel[2] * current_factor;
+                }
+
+            uchar result_blue = (sum_blue > 255) ? 255 : ((sum_blue < 0) ? 0 : (static_cast<float>(sum_blue) / divider) + offset);
+            uchar result_green = (sum_green > 255) ? 255 : ((sum_green < 0) ? 0 : (static_cast<float>(sum_green) / divider) + offset);
+            uchar result_red = (sum_red > 255) ? 255 : ((sum_red < 0) ? 0 : (static_cast<float>(sum_red) / divider) + offset);
+
+            candidate->at<cv::Vec4b>(i, j) = { result_blue, result_green, result_red, mat.at<cv::Vec4b>(i, j)[3] };
+        }
+
+    return candidate;
+}
+
+bool check(int i, int j, int current_coords, int max_row, int max_col) {
+    std::pair<int, int> modifier = coordinates[current_coords];
+    std::pair<int, int> new_coords = { (i + modifier.first), (j + modifier.second) };
+    return (0 <= new_coords.first && new_coords.first <= max_row) && (0 <= new_coords.second && new_coords.second <= max_col);
 }
 
 const_mat_ptr flou_gaussien(const_mat_ref mat) {
-    return apply(mat, std::vector({ 1, 1, 1, 1, 1, 1, 1, 1, 1 }), 16.0f, 0.0f);
+    return apply(mat, std::vector({ 1, 2, 1, 2, 4, 2, 1, 2, 1 }), 16.0f, 0.0f);
+}
+
+const_mat_ptr flou_box(const_mat_ref mat) {
+    return apply(mat, std::vector({ 1, 1, 1, 1, 1, 1, 1, 1, 1 }), 9.0f, 0.0f);
 }
 
 const_mat_ptr detection_bord(const_mat_ref mat) {
