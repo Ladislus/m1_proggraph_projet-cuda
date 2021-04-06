1ecdffd5b2bf0d1d404a2c020ac581714367ec6a
Ladislus <walcak.ladislas@gmail.com>
2021-04-06 19:52:35 +0200

    [MOD] Improved test script

--- test.sh
+++ test.sh
@@ -1,13 +1,32 @@
-if [ ! -d "cmake-build-debug" ]; then
-  mkdir "cmake-build-debug"
+echo 'Checking for directory "cmake-build-debug"...';
+if [ ! -d 'cmake-build-debug' ]; then
+  mkdir 'cmake-build-debug';
+  echo 'Created !';
+else
+  echo 'OK';
 fi
-if [ ! -d "output" ]; then
-  mkdir "cmake-build-debug"
+echo 'Checking for directory "output"...';
+if [ ! -d 'output' ]; then
+  mkdir 'cmake-build-debug';
+  echo 'Created !';
+else
+    echo 'OK';
 fi
-cd "cmake-build-debug" || exit;
+echo 'Moving to "cmake-build-debug"...';
+cd 'cmake-build-debug' || exit;
+echo 'Done';
+echo 'Generating CMake configuration';
 cmake ..;
-make
-./grayscale_cpu ../grayscale/Rue.png
-cp grayscale_cpu.png ../output
-./grayscale_gpu ../grayscale/Rue.png
-cp grayscale_gpu.png ../output
+echo 'Cmake done';
+echo 'Compiling';
+make;
+echo 'Cmake done';
+echo 'Executing "grayscale_cpu"';
+time ./grayscale_cpu ../grayscale/Rue.png;
+echo 'Copying outpout file';
+cp grayscale_cpu.png ../output;
+echo 'Executing "grayscale_gpu"';
+time ./grayscale_gpu ../grayscale/Rue.png;
+echo 'Copying outpout file';
+cp grayscale_gpu.png ../output;
+echo 'TESTS DONE';
