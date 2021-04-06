0599d83c6fe907084a08825f9889c3b805551b7a
Ladislus <walcak.ladislas@gmail.com>
2021-04-06 15:19:44 +0200

    [ADD] Added comments for ASCII art

--- asciiart/include/ascii_cpu.hpp
+++ asciiart/include/ascii_cpu.hpp
@@ -7,7 +7,12 @@
 void process(const_mat_ref, std::ofstream&);
 char convert_intensity(uchar);
 
+// Set de caractères simplifiés, qui fait perdre pas mal d'informations sur l'image
+//const std::string chars = " .:-=+*#%@";
+// Set de caractères plus complexe, mais qui est beaucoup plus sensible au bruit
 const std::string chars = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+
+// Constante diviseur pour convertir les pixels 1 channel en caractère ASCII
 const float divider = 255.0f / static_cast<float>(chars.size() - 1);
 
 #endif
--- asciiart/src/ascii_cpu.cpp
+++ asciiart/src/ascii_cpu.cpp
@@ -2,33 +2,52 @@
 
 int main(int argc, char** argv) {
 
+    // Chemin vers le fichier source manquant
     if (argc != 2) missing_argument();
 
+    // Récupération de l'image
     cv::Mat image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
 
+    // Image vide
     if (image.empty() || !image.data) missing_data();
 
+    // Ouverture du stream vers le fichier de sortie
     std::ofstream output("ascii_cpu.txt");
+    // Convertion de l'image, et écriture dans le fichier de sortie
     process(image, output);
 
+    // Fermeture du stream de sortie
     output.close();
 
     return EXIT_SUCCESS;
 }
 
+/**
+ * Fonction permettant d'obtenir le caractère ASCII correspondant à l'intensité donnée
+ * @param intensity L'intensité du caractère à convertie
+ * @return Le caractère ASCII correspondant
+ */
 char convert_intensity(uchar intensity) {
+    // Convertion de l'intensité en indice dans le set de caractère
     int rounded = static_cast<int>(static_cast<float>(intensity) / divider);
+    // Vérification que l'indice n'est pas OOB
     assert(rounded < chars.size());
+    // Retourne le caractère correspondant
     return chars[rounded];
 }
 
 void process(const_mat_ref image, std::ofstream& output) {
 
+    // Parcours des ligne
     for (size_t row = 0; row < image.rows; row++) {
+        // Parcours de colonnes
         for (size_t col = 0; col < image.cols; col++) {
+            // Récupération du caractère correpondant au pixel actuel
             uchar converted_char = convert_intensity(image.at<uchar>(row, col));
+            // Écriture dans le fichier de sortie
             output << converted_char << converted_char << converted_char;
         }
+        // Ajout du fin de ligne
         output << std::endl;
     }
 }
