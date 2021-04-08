b9a76a1652c35838240a409453e4798f279e3dcd
Ladislus <walcak.ladislas@gmail.com>
2021-04-08 13:56:14 +0200

    [TST]

--- convolution/src/convolution.cu
+++ convolution/src/convolution.cu
@@ -46,7 +46,7 @@ void device_apply(const uchar* data, uchar* candidate, size_t rows, size_t cols,
          int sum_green = 0;
          int sum_red = 0;
 
-         printf("[%d,%d]\n", i, j);
+         printf("[%d,%d] (row: %zu; col:%zu)\n", i, j, rows, cols);
 
          // Pour chacun des 9 cases dans son voisinage...
          // (size_t provoque des "narrow conversion")
