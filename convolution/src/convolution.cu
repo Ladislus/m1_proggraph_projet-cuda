9a4136762dd6c34837d732fc404c3387db0968b5
Ladislus <walcak.ladislas@gmail.com>
2021-04-08 14:12:54 +0200

    [TST]

--- convolution/src/convolution.cu
+++ convolution/src/convolution.cu
@@ -56,11 +56,11 @@ void device_apply(const uchar* data, uchar* candidate, size_t rows, size_t cols,
                  int current_factor = kernel[current_neighbor_index];
                  // Calcul des coordonnées du pixel à trouver
 
-                 printf("[%d,%d] (row: %ld; col:%ld) {divider: %f, offset: %f} - current index: %ld, Kernel[index]: %d, factor: %d\n", i, j, rows, cols, divider, offset, current_neighbor_index, kernel[current_neighbor_index], current_factor);
-
                  int new_x = static_cast<int>(i) + device_coordinates[current_neighbor_index][0];
                  int new_y = static_cast<int>(j) + device_coordinates[current_neighbor_index][1];
-//                 printf("[%d; %d] nighbor:%d OK factor:%d nx:%d ny:%d\n", i, j, current_neighbor_index, current_factor, new_x, new_y);
+
+                 printf("[%d,%d] (row: %ld; col:%ld) {divider: %f, offset: %f} - current index: %ld, Kernel[index]: %d, factor: %d, (%d, %d)\n", i, j, rows, cols, divider, offset, current_neighbor_index, kernel[current_neighbor_index], current_factor, new_x, new_y);
+
 
                  if (new_x >= cols || new_x < 0 || new_y < 0 || new_y >= rows) printf("ERROR : [%d; %d]\n", i, j);
 
